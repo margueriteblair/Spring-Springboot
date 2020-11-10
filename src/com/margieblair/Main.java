e52df625b32723810aadcefefd166e877958f962
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-10 13:00:02 -0500

    cleanup to start spring course

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -3,20 +3,7 @@ package com.margieblair;
 public class Main {
 
     public static void main(String[] args) {
-	    int i = 010;
-	    int j = 07;
-        System.out.println(i);
-        System.out.println(j);
 
-        try {
-            Float f1 = new Float("3.0");
-            int a = f1.intValue();
-            byte b = f1.byteValue();
-            double c = f1.byteValue();
-            System.out.println(a + b + c);
-        } catch (NumberFormatException e) {
-            System.out.println("Bad number");
-        }
     }
 }
 
