d7f03a1c5b0e900aafef963065858f6e935b7309
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-10 12:32:54 -0500

    algorithm work nothing on spring yet

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -3,6 +3,20 @@ package com.margieblair;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int i = 010;
+	    int j = 07;
+        System.out.println(i);
+        System.out.println(j);
+
+        try {
+            Float f1 = new Float("3.0");
+            int a = f1.intValue();
+            byte b = f1.byteValue();
+            double c = f1.byteValue();
+            System.out.println(a + b + c);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad number");
+        }
     }
 }
+
